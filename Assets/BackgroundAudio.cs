a4968f48b9beeafa18b23b85d44eb0c0d4390688
maidongthaini <Elevan1227@gmail.com>
2024-04-23 15:40:40 -0400

    audio scripting

---
+++ Assets/BackgroundAudio.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource musicSource;
+    [SerializeField]
+    private AudioSource drivingSource;
+    [SerializeField]
+    private AudioClip music;
+    [SerializeField]
+    private AudioClip drive;
+
+    void Start()
+    {
+        loopAudio(audio);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void setLoopAudio(AudioSource source, AudioClip audio)
+    {
+        source.loop = true;
+        source.clip = audio;
+        source.Play();
+    }
+
+    public void stopMusic()
+    {
+        musicSource.Stop();
+    }
+}
