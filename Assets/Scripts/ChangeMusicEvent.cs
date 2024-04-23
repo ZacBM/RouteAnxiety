362329c4cad8d38198f4e0cd0c950324aa55eeb5
Henry Nguyen <hn437@drexel.edu>
2024-04-23 15:13:43 -0400

    Added change music interaction event w/ run()

---
+++ Assets/Scripts/ChangeMusicEvent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicEvent : InteractionEvent
+{
+    public ChangeMusicEvent() : base() { }
+
+    public override void run()
+    {
+        base.run();
+
+    }
+}
