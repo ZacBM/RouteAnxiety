73868e5de018221e3501343fa043285a1ad6382e
maidongthaini <Elevan1227@gmail.com>
2024-04-24 16:51:01 -0400

    Background audio overhaul

--- Assets/Scripts/BackgroundAudio.cs
+++ Assets/Scripts/BackgroundAudio.cs
@@ -9,29 +9,33 @@ public class BackgroundAudio : MonoBehaviour
     [SerializeField]
     private AudioSource drivingSource;
     [SerializeField]
-    private AudioClip music;
-    [SerializeField]
-    private AudioClip music2;
-    [SerializeField]
-    private AudioClip drive;
-    [SerializeField]
-    private int seconds;
+    private List<AudioClip> tracks;
+    private System.Random random = new System.Random();
+    private int index;
     private int i;
+    private float musicVolume;
+
     void Start()
     {
-        setLoopAudio(musicSource, music);
-        setLoopAudio(drivingSource, drive);
+        index = random.Next(2, tracks.Count);
+        setLoopAudio(musicSource, tracks[index]);
+        setLoopAudio(drivingSource, tracks[1]);
+        musicVolume = .2f;
+        musicSource.volume = musicVolume;
+        drivingSource.volume = .75f;
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        if (i / 60 == seconds)
+        if (i / 60 == 7)
         {
-            switchMusic(music2);
+            switchMusic();
+            i = 0;
         }
     }
+
     public void setLoopAudio(AudioSource source, AudioClip audio)
     {
         source.loop = true;
@@ -39,11 +43,29 @@ public class BackgroundAudio : MonoBehaviour
         source.Play();
     }
 
-    public void switchMusic(AudioClip audio)
+    public void switchMusic()
     {
+        int newSong = random.Next(2, tracks.Count);
+        while (newSong == index)
+        {
+            newSong = random.Next(2, tracks.Count);
+        }
+        index = newSong;
 
+        musicSource.volume = 1f;
+        musicSource.loop = false;
+        musicSource.clip = tracks[0];
+        musicSource.Play();
+
+        StartCoroutine(PlayNextTrack());
+    }
+
+    IEnumerator PlayNextTrack()
+    {
+        yield return new WaitForSeconds(musicSource.clip.length);
+        musicSource.volume = musicVolume;
         musicSource.loop = true;
-        musicSource.clip = audio;
+        musicSource.clip = tracks[index];
         musicSource.Play();
     }
 }
