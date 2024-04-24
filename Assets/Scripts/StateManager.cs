c238c0f9dd98f3df8e7d752c8dc125c6af76233d
Henry Nguyen <hn437@drexel.edu>
2024-04-23 21:51:21 -0400

    Updated state manager in combination with anxiety

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -6,15 +6,38 @@ public class StateManager
 {
     private const float defaultAnxiety = 0f;
     private float anxiety = 0f;
+    private readonly uint winningDecisions;
+    private uint decisions;
 
-    public StateManager(float anxiety = defaultAnxiety)
+    public StateManager(uint winningDecisions)
     {
-        this.anxiety = anxiety;
+        this.winningDecisions = winningDecisions;
     }
 
-    public void changeAnxiety(float anxiety)
+    public bool won()
+    {
+        return anxiety > 0f && anxiety < 100f && decisions >= winningDecisions;
+    }
+
+    public bool lost()
+    {
+        return anxiety == 0f || anxiety == 100f;
+    }
+
+    public void addDecision()
+    {
+        decisions++;
+    }
+    
+    private void changeAnxiety(float anxiety)
     {
         this.anxiety = Mathf.Clamp(anxiety, 0f, 100f);
+    }
+
+    public void processDecision(float anxietyIncrement)
+    {
+        incrementAnxiety(anxietyIncrement);
+        addDecision();
     }
 
     public float getAnxiety()
