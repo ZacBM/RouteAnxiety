9ce7c89baeb1684862ffe0793cb0d6a2fe937618
Henry Nguyen <hn437@drexel.edu>
2024-04-26 12:37:58 -0400

    Implemented random timed spawning
    But unfinished

--- Assets/Scripts/First.cs
+++ Assets/Scripts/First.cs
@@ -6,6 +6,10 @@ using UnityEngine.UIElements;
 public class First : MonoBehaviour
 {
     [SerializeField]
+    private float secondsUntilSpawn = 2f;
+    private float timeCount;
+
+    [SerializeField]
     private const int winningDecisions = 50; // default set here
 
     private StateManager stateManager;
@@ -16,8 +20,11 @@ public class First : MonoBehaviour
         stateManager = new StateManager(winningDecisions);
         interactionEventSpawner = new InteractionEventSpawner();
         interactionEventSpawner.addInteractionEvent(new ChangeMusic(stateManager, 2f)); // HARD CODED 2f for now
+        interactionEventSpawner.addInteractionEvent(new ChangeLane(stateManager, 2f)); // HARD CODED 2f for now
+        interactionEventSpawner.addInteractionEvent(new SpeedChange(stateManager, 2f)); // HARD CODED 2f for now
+        interactionEventSpawner.addInteractionEvent(new WindowChange(stateManager, 2f)); // HARD CODED 2f for now
     }
-    
+
     public StateManager getManager()
     {
         return stateManager;
@@ -31,6 +38,14 @@ public class First : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float now = Time.deltaTime;
+        if (timeCount >= secondsUntilSpawn)
+        {
+            timeCount = 0;
+            // start spawning a random event
+        } else
+        {
+            timeCount += now;
+        }
     }
 }
