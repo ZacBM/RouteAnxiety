03b3d8880ad99559b1c2055b6e6730f8d0f24557
Henry Nguyen <hn437@drexel.edu>
2024-04-28 02:10:07 -0400

    Finished binary speed change

--- Assets/Scripts/InteractionEvents/SpeedChange.cs
+++ Assets/Scripts/InteractionEvents/SpeedChange.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class SpeedChange: InteractionEvent
 {
     private const float defaultAnxietyAmount = 2f;
+    private int[] speedList = new int[2];
+    private int currentIndex = 0;
+    private InfiniteRoad road;
 
-    public SpeedChange(StateManager stateManager, float anxietyAmount = defaultAnxietyAmount): base(stateManager, anxietyAmount)
+    private void scrollIndex()
     {
-        
+        currentIndex++;
+        if(currentIndex == speedList.Length)
+        {
+            currentIndex = 0;
+        }
+    }
+
+    public SpeedChange(StateManager stateManager, InfiniteRoad road, float anxietyAmount = defaultAnxietyAmount): base(stateManager, anxietyAmount)
+    {
+        this.road = road;
+        this.speedList[0] = road.getSpeed(); // tries to use serialized speed
+        this.speedList[1] = road.getSpeed() + 100; // adds 100 to it (arbritrary)
     }
 
     public override void run()
     {
         base.run();
         // do stuff here
+        scrollIndex();
+        road.setSpeed(this.speedList[currentIndex]);
     }
 }
