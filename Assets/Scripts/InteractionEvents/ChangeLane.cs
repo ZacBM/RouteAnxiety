f77030c744084068af7fee1a7ff48b1498b43846
Henry Nguyen <hn437@drexel.edu>
2024-04-28 02:09:23 -0400

    Completed binary lane switch code

--- Assets/Scripts/InteractionEvents/ChangeLane.cs
+++ Assets/Scripts/InteractionEvents/ChangeLane.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ChangeLane : InteractionEvent
 {
+    private bool rightLane = false;
     private const float defaultAnxietyAmount = 2f;
+    private Vector3 firstLane;
+    private Vector3 secondLane;
+    private CarController carController;
 
-    public ChangeLane(StateManager stateManager, float anxietyAmount = defaultAnxietyAmount) : base(stateManager, anxietyAmount)
+    public ChangeLane(StateManager stateManager, CarController carController, float anxietyAmount = defaultAnxietyAmount) : base(stateManager, anxietyAmount)
     {
-
+        this.carController = carController;
+        firstLane = carController.getTransform().position;
+        secondLane = firstLane + new Vector3(carController.getTransform().localScale.x, 0, 0);
+        secondLane = new Vector3(secondLane.x * 10, secondLane.y, secondLane.z);
     }
 
     public override void run()
     {
         base.run();
         // do stuff here
+        rightLane = !rightLane;
+        carController.lerpCarToPosition(rightLane ? firstLane : secondLane);
     }
 }
